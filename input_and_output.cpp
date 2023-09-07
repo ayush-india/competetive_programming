95e66c37d99dade32898a8a0bfc5357115aed6c1
ayushmishra <mail81mitthu@gmail.com>
2023-09-07 15:59:20 +0530

    m

---
+++ input_and_output.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int a,b;
+  string x;
+
+  printf("enter the input\n");
+
+  cin >> a >> b >> x;
+
+  printf("{a}\n");
+  // count << a << " " << b << " " << x;
+  //
+  // 
+  // int a = 123, b = 456;
+  // string x = "monkey";
+  // cout << a << " " << b << " " << x << "\n";
+  return 0;
+}
