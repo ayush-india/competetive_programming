081c1df088bbc517f42889d498db6d94e001d939
ayushmishra <mail81mitthu@gmail.com>
2023-12-26 16:27:55 +0530

    div 4 https://codeforces.com/contest/1873/

---
+++ problems/shortshort.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/contest/1873/problem/A
+// see div4/one.cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) { 
+
+  }
+   
+}
